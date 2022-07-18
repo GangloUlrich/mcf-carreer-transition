7f06ecb1626c7a010af43f3ae56f2e270d0f3715
GANGLO Ulrich <gangloulrichluc@gmail.com>
2022-07-18 16:43:10 +0100

    page d'accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -2,51 +2,27 @@
 
 @section('content')
 
-    <section class="min-vh-100 bg-dark text-center">
-        <div class="container p-5">
-            <h1 class="display-2 text-white fw-bold ">MCF CARREER TRANSITION PROJECT</h1>
+    <div class="container-fluid" id="hero">
+        <h1 class="text-white text-center">MCF CARREER TRANSITION PLATFORM</h1>
+        <div class="text-center text-white">Répertoire des intérêts des boursiers pour la transition vers l'emploi</div>
+    </div>
 
-            <div class="col-12">
-                <div class="row justify-content-center gap-4">
+    <div class="container">
 
+        <div class="d-flex flex-column align-items-center  gap-5 ">
+            <div class="col-xl-8 col-12 text-center pt-5">
+                Cette liste permet d'avoir une base de données exhaustive des informations d'ordre académiques et
+                professionnelles, ainsi que des intérêts et besoins d'accompagnements de tous les boursiers MCF pour la
+                transition vers l'emploi. Merci de faciliter la constitution de cette base de données et de contribuer à
+                la création d'une offre de services pour la transition adaptée à vos besoins respectifs.
+            </div>
 
-                    <div>
-                        <a href="{{ route('login') }}" class="btn btn-warning w-50 me-3">Login</a>
-                    </div>
-
-                    <div>
-                        <a href="{{ route('register') }}" class="btn btn-success w-50">Register</a>
-                    </div>
-
-
-
-                    <div class="container">
-                        <form action="">
-
-                            <div>
-                                <input type="text" class="form-control" name="">
-                            </div>
-
-                            <div>
-                                <input type="text" class="form-control" name="" placeholder="Email">
-                            </div>
-
-                            <div>
-                                <input type="date" class="form-control" name="" placeholder="Date de naissance">
-                            </div>
-
-                            <div>
-                                <input type="text" class="form-control" name="">
-                            </div>
-
-                        </form>
-                    </div>
-
-
-                </div>
+            <div>
+                <a href="{{ route('register') }}" class="nav-btn-primary">S'inscrire <i class="fa-solid fa-arrow-right"></i></a>
             </div>
         </div>
 
-    </section>
+    </div>
 
 @endsection
+
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,11 +7,39 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MCF CARREER TRANSITION</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/web.css') }}">
 </head>
 <body>
 
-@yield('content')
 
+
+<header class="container-fluid bg-white">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+
+
+                <a href="https://mcfsp-uac.bj/" target="_blank" id="headerLink"><img src="{{ asset('img/logo.webp') }}" alt="MCFSP-UAC" class="logo"></a>
+
+                <nav class="d-none d-md-flex nav">
+                    <a class="nav-link active nav-btn-primary  " aria-current="page" href="#">Commencer <i class="fa-solid fa-arrow-right"></i></a>
+                    <a class="nav-link nav-btn-secondary" href="{{ route('login') }}">Connexion <i class="fa-solid fa-right-to-bracket"></i></a>
+
+                </nav>
+
+
+        </div>
+    </div>
+
+</header>
+
+<main>
+    @yield('content')
+</main>
+
+<footer class="container">
+
+</footer>
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
 </body>
